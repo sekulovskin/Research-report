3a559068cca054b2c21aa2578260719149fdc015
sekulovskin <n.sekulovski@students.uu.nl>
2021-12-10 16:38:23 +0100

    New commit

--- Sensitivity_analysis.R
+++ Sensitivity_analysis.R
@@ -1,26 +1,45 @@
-######## Analysis presented in the Research Report ########
-#Sensitivity of Null Hypothesis Bayesian Testing in the context of two-level models
+#######################################################################################
+######## Analysis presented in the Research Report ###################################
+#####################################################################################
+#Sensitivity of Null Hypothesis Bayesian Testing in the context of two-level models#
+###################################################################################
 
-#IMPORTANT: set a working directory where the downloaded folder `code` is stored
+#IMPORTANT: 
+
+  #1 set a working directory where the downloaded folder from GitHub is stored
+
+#setwd("...")
+#OR set it manually by clicking: Session -> Set Working Directory -> Choose Directory...
+
+  #2 In case (some of) the packages below do not load uncomment and run (some of) the following code:
+
+#install.packages("foreign")
+#install.packages("bain")
+#install.packages("lme4")
+#install.packages("jtools")
+#install.packages("psych")
+
 
 # Load the required packages -------------------------------------
 
 library(foreign) #for loading the data
 library(bain) #the wrapper function does not work without loading bain
-source("wrapper_function.R") # load the wrapper function
 library(lme4) #for multilevel modeling
 library(jtools) #for nice summaries of lmer objects
-library(psych) #for nice descriptives
-
+library(psych) #for nice descriptive statistics 
+source("wrapper_function.R") # load the wrapper function
 
 # Load the data -----------------------------------------------------------
 
 dat <- read.spss(file = "tutorial.sav", to.data.frame = T)
 
 #Additionally the data can be loaded directly from the `R2MLwiN` package, BUT NOTE
-# in this case the data would contain many more variables (not used in this analysis)
+#in that case the data would contain many more variables (not used in this analysis)
 #and additionally the naming of the variables is slightly different, here we will use
 #the data loaded above
+
+
+#If the reader wants to load the data directly from the `R2MLwiN` package (not advised):
 
 #library(R2MLwiN)
 #data(tutorial)
@@ -29,7 +48,7 @@ dat <- read.spss(file = "tutorial.sav", to.data.frame = T)
 
 describe(dat)
 
-# Fit the two-level models using lmer -------------------------------------
+# Fit the two-level model using lmer -------------------------------------
 
 x <- lmer(Examscore ~ 1 + LRTscore + AvsLRT + (LRTscore | School), 
           REML = F, data = dat)
@@ -38,27 +57,35 @@ summ(x)  #for nicer output
 
 
 
-# Use the wrapper to test the hypotheses using the default value for b ----------
-# In this case 1*b (where b = 2/65 = 0.03)
-mlm_bain_std(x, "LRTscore = AvsLRT = 0;
-         LRTscore = AvsLRT; LRTscore > 0 & AvsLRT >0", 
-             fraction = 1, standardize = FALSE, seed = 123)
+# SENSITIVITY ANALYSIS ----------------------------------------------------
 
 
-# SENSITIVITY ANALYSIS ---------------------------------------------------
+# Use the wrapper to test the hypotheses using the default value for b 
+#We use `standardize = FALSE`, since the data is already standardized by the original authors
+#`N = "level_2" ` indicates that we will use the number of level 2 observations as the sample size 
+#We use `fraction = 1`
+#All of this means we apply: `b` = 2/65 = 0.03 (since, b = J/N), i.e., 1 * 0.03
+bain_2lmer(x, "LRTscore = AvsLRT = 0;
+         LRTscore = AvsLRT; LRTscore > 0 & AvsLRT >0", 
+             fraction = 1, standardize = FALSE, N = "level_2" ,seed = 123)
 
-# 2 * b
-mlm_bain_std(x, "LRTscore = AvsLRT = 0;
+
+# Iteratively change the value of the argument `fraction`
+
+#`fraction = 2`, i.e., 2 * b
+bain_2lmer(x, "LRTscore = AvsLRT = 0;
          LRTscore = AvsLRT; LRTscore > 0 & AvsLRT >0", 
-             fraction = 2, standardize = FALSE, seed = 123)
-# 3 * b
-mlm_bain_std(x, "LRTscore = AvsLRT = 0;
+           fraction = 2, standardize = FALSE, N = "level_2" ,seed = 123)
+
+#`fraction = 3`, i.e., 3 * b
+bain_2lmer(x, "LRTscore = AvsLRT = 0;
          LRTscore = AvsLRT; LRTscore > 0 & AvsLRT >0", 
-             fraction = 3, standardize = FALSE, seed = 123)
-# 4 * b
-mlm_bain_std(x, "LRTscore = AvsLRT = 0;
+           fraction = 3, standardize = FALSE, N = "level_2" ,seed = 123)
+
+#`fraction = 4`, i.e., 4 * b
+bain_2lmer(x, "LRTscore = AvsLRT = 0;
          LRTscore = AvsLRT; LRTscore > 0 & AvsLRT >0", 
-             fraction = 4, standardize = FALSE, seed = 123)
+           fraction = 4, standardize = FALSE, N = "level_2" ,seed = 123)
 
 # Obtain the plot in the paper --------------------------------------------
 
@@ -68,10 +95,10 @@ BF_i <- rep(4.21, 4)
 
 # plot
 par(mfrow = c(1, 2)) 
-plot(BF_o, type = "l",  xaxt = "n", xlab = "times b", yaxt = "n", ylab = "BF", main = "a")
+plot(BF_o, type = "l",  xaxt = "n", xlab = "times b", yaxt = "n", ylab = "BF", main = "(a)")
 axis(1, at = 1:4)
 axis(2, at = BF_o)
-plot(BF_i, type = "l", xaxt = "n", yaxt = "n", xlab = "times b", ylab = "BF", main = "b")
+plot(BF_i, type = "l", xaxt = "n", yaxt = "n", xlab = "times b", ylab = "BF", main = "(b)")
 axis(1, at = 1:4)
 axis(2, at = BF_i)
 
--- wrapper_function.R
+++ wrapper_function.R
@@ -1,79 +1,152 @@
-mlm_bain_std <- function(x, hypotheses, fraction, standardize = FALSE, type.of.stand
-                         = c("overall", "within"), seed){
+bain_2lmer <- function(x, hypotheses, fraction, standardize = FALSE, 
+                       N = c("level_1", "level_2", "ICC_effective"), seed){
+ 
   n_vas <- names(x@frame) #extract names of all variables in the data
-  N <- nrow(coef(x)[[1]]) #sample size = level 2 observations
-  #--------------------------------------
-  #Unstandardized
   estimates <- coef(x) #extract the ML (unstandardized) "random" effects 
   estimates <- as.data.frame(estimates[[1]])
   estimates <- estimates[ , -1] #remove the intercept
-  fixed_estimates <- apply(estimates, 2, mean) # obtain fix effects 
-  cov <- as.matrix(vcov(x)[-1, -1]) #obtain the covariance matrix of the 
-  #--------------------------------------
-  #Overall standardization of the data "overall predictors"`
-  standardized <- apply(x@frame, 2, scale)
-  standardized <- as.data.frame(standardized)
-  standardized[, ncol(standardized)] <- x@frame[ , ncol(x@frame)] #group
-  names(standardized) <- n_vas #add the same names
-  s <- lmer(x@call$formula, data = standardized, REML = FALSE)
-  estimates_s <- coef(s) #extract the ML (unstandardized) "random" effects 
-  estimates_s <- as.data.frame(estimates_s[[1]])
-  estimates_s <- estimates_s[ , -1] #remove the intercept
-  fixed_estimates_s <- apply(estimates_s, 2, mean) # obtain fix effects 
-  #names(fixed_estimates_s) <- n #add names
-  cov_s <- as.matrix(vcov(s)[-1, -1]) #obtain the covariance matrix of the 
-  #--------------------------------------
-  #Within group standardization of the (lvl1) predictors "within group predictors"
-  y <- ave(x@frame[, 1], x@frame[, 4], FUN = scale) # standardize the outcome
-  x1 <- ave(x@frame[, 2], x@frame[, 4], FUN = scale) # standardize the lvl1 pred
-  # for the level 2 predictor: ------------
-  split_group <- split(x@frame, x@frame[,4]) 
-  x_lvl2 <- matrix(nrow = nrow(x@frame), ncol = 1)
-  for (i in seq_along(split_group)){
-    x_lvl2[i, ] <- split_group[[i]][1,3]
-  }
-  x_lvl2 <- scale(x_lvl2)  # standardize
-  n_gr <- sapply(split_group, nrow)  #obtain size of each group
-  x_lvl2_full <- suppressWarnings(rep(x_lvl2, each = n_gr))
-  gr <- x@frame[ , ncol(x@frame)]  #factor for groups
-  #------------------
-  dat <- data.frame(y, x1, x_lvl2_full, gr) #combine in a data frame
-  z <- lmer(y ~ 1 + x1 + x_lvl2_full + (0 + x1|gr), data = dat, REML = F) #improved
-  estimates_z <- coef(z) #extract the ML (unstandardized) "random" effects 
-  estimates_z <- as.data.frame(estimates_z[[1]])
-  estimates_z <- estimates_z[ , -1] #remove the intercept
-  fixed_estimates_z <- apply(estimates_z, 2, mean) # obtain fix effects
-  n <- names(estimates) #extrct the names of parameters  
-  names(fixed_estimates_z) <- n #add names  I ONLY NEED THIS FOR NOW
-  cov_z <- as.matrix(vcov(z)[-1, -1]) #obtain the covariance matrix 
-  #---------------------------------------  
-  #Compute BF's  
   
+  #--------------------------------------
+  
+if(N == "level_1"){
+    
+    N <- nrow(x@frame) #sample size = level 1 observations
+    
   if(standardize == FALSE){ 
-    set.seed(seed)
-    results <- bain(fixed_estimates, hypotheses, n = N, Sigma = cov, 
-                    group_parameters = 0, 
-                    joint_parameters = length(estimates), 
-                    fraction = fraction)
-  }
-  
-  else{
-    
-    
-    if(type.of.stand == "within"){
-      set.seed(seed)
-      results <- bain(fixed_estimates_z, hypotheses, n = N, Sigma = cov_z,
-                      group_parameters = 0, joint_parameters = length(estimates), 
+      
+       #Unstandardized data
+       fixed_estimates <- apply(estimates, 2, mean) # obtain fix effects 
+       cov <- as.matrix(vcov(x)[-1, -1]) #obtain the covariance matrix of the 
+       #--------------------------------------
+      
+       set.seed(seed)
+       results <- bain(fixed_estimates, hypotheses, n = N, Sigma = cov, 
+                      group_parameters = 0, 
+                      joint_parameters = length(estimates), 
                       fraction = fraction)
     }
     
-    if(type.of.stand == "overall"){
+    else{
+      
+        #Overall standardization of the data "overall predictors"`
+        standardized <- apply(x@frame, 2, scale)
+        standardized <- as.data.frame(standardized)
+        standardized[, ncol(standardized)] <- x@frame[ , ncol(x@frame)] #group
+        names(standardized) <- n_vas #add the same names
+        s <- lmer(x@call$formula, data = standardized, REML = FALSE)
+        estimates_s <- coef(s) #extract the ML (unstandardized) "random" effects 
+        estimates_s <- as.data.frame(estimates_s[[1]])
+        estimates_s <- estimates_s[ , -1] #remove the intercept
+        fixed_estimates_s <- apply(estimates_s, 2, mean) # obtain fix effects 
+        #names(fixed_estimates_s) <- n #add names
+        cov_s <- as.matrix(vcov(s)[-1, -1]) #obtain the covariance matrix of the 
+        #--------------------------------------
+        
+        set.seed(seed)
+        results <- bain(fixed_estimates_s, hypotheses, n = N, Sigma = cov_s,
+                        group_parameters = 0, joint_parameters = length(estimates), 
+                        fraction = fraction)
+    }
+    
+  }
+  
+  
+if(N == "level_2"){
+    
+    N <- nrow(coef(x)[[1]]) #sample size = level 2 observations
+    
+  if(standardize == FALSE){ 
+      
+        #Unstandardized data
+        estimates <- coef(x) #extract the ML (unstandardized) "random" effects 
+        estimates <- as.data.frame(estimates[[1]])
+        estimates <- estimates[ , -1] #remove the intercept
+        fixed_estimates <- apply(estimates, 2, mean) # obtain fix effects 
+        cov <- as.matrix(vcov(x)[-1, -1]) #obtain the covariance matrix of the 
+      #--------------------------------------
+      
+        set.seed(seed)
+        results <- bain(fixed_estimates, hypotheses, n = N, Sigma = cov, 
+                      group_parameters = 0, 
+                      joint_parameters = length(estimates), 
+                      fraction = fraction)
+  }
+    
+    else{
+      
+        #Overall standardization of the data "overall predictors"`
+        standardized <- apply(x@frame, 2, scale)
+        standardized <- as.data.frame(standardized)
+        standardized[, ncol(standardized)] <- x@frame[ , ncol(x@frame)] #add grouping factor
+        names(standardized) <- n_vas #add the same names
+        s <- lmer(x@call$formula, data = standardized, REML = FALSE)
+        estimates_s <- coef(s) #extract the ML (unstandardized) "random" effects 
+        estimates_s <- as.data.frame(estimates_s[[1]])
+        estimates_s <- estimates_s[ , -1] #remove the intercept
+        fixed_estimates_s <- apply(estimates_s, 2, mean) # obtain fix effects 
+        #names(fixed_estimates_s) <- n #add names
+        cov_s <- as.matrix(vcov(s)[-1, -1]) #obtain the covariance matrix of the 
+        #--------------------------------------
+        
+        set.seed(seed)
+        results <- bain(fixed_estimates_s, hypotheses, n = N, Sigma = cov_s,
+                        group_parameters = 0, joint_parameters = length(estimates), 
+                        fraction = fraction)
+    }
+      
+  }
+  
+if(N == "ICC_effective"){
+    
+    N_lvl1 <- nrow(x@frame)
+    gr <- x@frame[ , ncol(x@frame)] # extract grouping factor
+    split_gr <- split(x@frame, gr) #split with respect to the grouping variable
+    N_clus <- mean(sapply(split_gr, nrow)) # compute the average group size
+    model_0 <- lmer(x@frame[,1] ~ 1 + (1|gr)) # fit a random-intercept model
+    var <- as.data.frame(VarCorr(model_0)) #extract the variances of the random intercept and residuals
+    ICC <- var[1, 4]/ (var[1, 4] + var[2, 4]) #compute the ICC
+    #compute the effective sample size based on the ICC approach 
+    N <- N_lvl1 / (1 + (N_clus - 1) * ICC)
+    
+  if(standardize == FALSE){ 
+      
+       #Unstandardized data
+       estimates <- coef(x) #extract the ML (unstandardized) "random" effects 
+       estimates <- as.data.frame(estimates[[1]])
+       estimates <- estimates[ , -1] #remove the intercept
+       fixed_estimates <- apply(estimates, 2, mean) # obtain fix effects 
+       cov <- as.matrix(vcov(x)[-1, -1]) #obtain the covariance matrix of the 
+       #--------------------------------------
+      
+       set.seed(seed)
+       results <- bain(fixed_estimates, hypotheses, n = N, Sigma = cov, 
+                      group_parameters = 0, 
+                      joint_parameters = length(estimates), 
+                      fraction = fraction)
+    }
+    
+  else{
+      
+      #Overall standardization of the data "overall predictors"`
+      standardized <- apply(x@frame, 2, scale)
+      standardized <- as.data.frame(standardized)
+      standardized[, ncol(standardized)] <- x@frame[ , ncol(x@frame)] #group
+      names(standardized) <- n_vas #add the same names
+      s <- lmer(x@call$formula, data = standardized, REML = FALSE)
+      estimates_s <- coef(s) #extract the ML (unstandardized) "random" effects 
+      estimates_s <- as.data.frame(estimates_s[[1]])
+      estimates_s <- estimates_s[ , -1] #remove the intercept
+      fixed_estimates_s <- apply(estimates_s, 2, mean) # obtain fix effects 
+      #names(fixed_estimates_s) <- n #add names
+      cov_s <- as.matrix(vcov(s)[-1, -1]) #obtain the covariance matrix of the 
+      #--------------------------------------
+        
       set.seed(seed)
       results <- bain(fixed_estimates_s, hypotheses, n = N, Sigma = cov_s,
-                      group_parameters = 0, joint_parameters = length(estimates), 
-                      fraction = fraction)
-    }
-    
-  }
+                        group_parameters = 0, joint_parameters = length(estimates), 
+                        fraction = fraction)
+      }
+      
+ }
   return(results)
 } 
